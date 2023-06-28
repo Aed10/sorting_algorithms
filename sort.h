a22693c9de070adc0be27997a061fa2cc93d9443
Aed10 <eddyyanyayoub@gmail.com>
2023-06-28 01:48:58 +0100

    Add initial configuration files and implement bubble sort algorithmThe commit adds the initial configuration files for VSCode and implements the bubble sort algorithm in C. The bubble sort algorithm sorts an array of integers in ascending order. The commit also includes the print_array and print_list functions, which are used to print arrays and lists, respectively

---
+++ sort.h
@@ -0,0 +1,33 @@
+#ifndef SORT_H
+# define SORT_H
+
+# include <stdio.h>
+# include <stdlib.h>
+
+/* Data structures */
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ *
+ * Description: Doubly linked list node structure
+ * for Holberton project
+ */
+
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+}	listint_t;
+
+/* Function prototypes */
+void	print_array(const int *array, size_t size);
+void	print_list(const listint_t *list);
+
+/* Mandatory tasks */
+void	bubble_sort(int *array, size_t size);
+
+#endif /* SORT_H */
